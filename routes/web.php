ddf5cc81ab3fa556e00e6d7f1f0aa9684be464e8
yassinemounzih <yassinemounzih9@gmail.com>
2020-09-16 12:05:26 +0100

    admin panel config done

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Controllers\CategoryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/',function(){
+
+    return view('website.home');
+
+});
+
+Route::get('/about',function(){
+
+    return view('website.about');
+
+});
+Route::get('/category',function(){
+
+    return view('website.category');
+
+});
+Route::get('/contact',function(){
+
+    return view('website.contact');
+
+});
+
+Route::get('/post',function(){
+
+    return view('website.post');
+
+});
+
+
+// Admin panel 
+
+Route::get('/ys' , [CategoryController::class ,'test']);
